ed5b2fb4db9817709039a68ec4e04441217331e9
Aditya <aditya.prajapati@nissisoftwaresystems.com>
2021-07-26 20:12:19 +0530

    day12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catelog/models/catalog.dart';
 import 'package:flutter_catelog/widgets/drawer.dart';
+import 'package:flutter_catelog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String value = "Tutorials test";
-    double pi = 3.14;
-    bool isMale = true;
-    num temp = 30.1; // it accepts both integers and double values.
-    var allValues = 'Hello'; // it accepts all typles of value.
-    const piValue = 3.14;
+    final dummyList = List.generate(100, (index) => CatelogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text('Catelog App'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Welcome to $days days flutter tutorials $value'),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummyList[index]);
+            }),
       ),
       drawer: Mydrawer(),
     );
